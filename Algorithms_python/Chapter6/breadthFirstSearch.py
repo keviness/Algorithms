6ed20dfde265bcbb51d5ee28b542a4d7399b390d
keviness <kevin1737@163.com>
2020-03-07 22:48:47 +0800

    add: DijkstraAlgorithm

---
+++ Algorithms_python/Chapter6/breadthFirstSearch.py
@@ -0,0 +1,23 @@
+'''breadth first search'''
+def search(name):
+    graph = {}
+    graph["you"] = ["kevin", "Bob"]
+    graph["kevin"] = ["Alice", "Lucy"]
+    graph["Bob"] = ["Nancy"]
+
+    search_queue = deque()
+    search_queue += graph[name]
+    searched = []
+    while search_queue:
+        person = search_queue.popleft()
+        if not person in searched:
+            if person_is_seller(person):
+                print(person+" is a mango seller!")
+                return True
+            else:
+                search_queue += graph[person]
+                searched.append(person)
+    return False
+
+def person_is_seller(name):
+    return name[-1] == "m"  #return true or false 
