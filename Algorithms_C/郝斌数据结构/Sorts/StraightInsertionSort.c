47c59efc98dd1504f8c7fd8e3df81b11392367f8
keviness <kevin1737@163.com>
2020-08-14 22:51:39 +0800

    Update: DataStruct Add: straight-insertion sort

---
+++ Algorithms_C/郝斌数据结构/Sorts/StraightInsertionSort.c
@@ -0,0 +1,46 @@
+/*** straight insertion sort ***/
+#include <stdio.h>
+#include <string.h>
+#define SIZE 10
+
+void StraightInsertionSort(int *arr);
+void ShowArray(int *arr);
+
+int main(void)
+{
+    int array[SIZE] = {0, 1, -23, 4, 5, 7, -18, -112, 29, 34};
+    puts("The array:");
+    ShowArray(array);
+    StraightInsertionSort(array);
+    puts("The array after calling sort function:");
+    ShowArray(array);
+
+    return 0;
+}
+
+void StraightInsertionSort(int *arr)
+{
+    int i, j;
+    
+    for (i=2; i<SIZE; i++)
+    {
+        if (arr[i] < arr[i-1])
+        {
+            arr[0] = arr[i];  //设置数组首元素为哨兵
+            for (j=i-1; arr[j]>arr[0]; j--)
+            {
+                arr[j+1] = arr[j];
+            }
+            arr[j+1] = arr[0];
+        }
+    }
+}
+
+void ShowArray(int *arr)
+{
+    for (int i=1; i<SIZE; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    putchar('\n');
+}
