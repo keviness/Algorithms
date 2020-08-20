1fc747035fc656f1d56823375916bb06df3c1440
keviness <kevin1737@163.com>
2020-08-20 23:17:39 +0800

    Add: merge sort

---
+++ Algorithms_C/郝斌数据结构/Sorts/MergeSort.c
@@ -0,0 +1,88 @@
+/*** Merge Sort ***/
+#include <stdio.h>
+#include <string.h>
+#define SIZE 10
+
+void Merge(int *arr, int left, int middle, int right);
+void MergeSort(int *arr, int left, int right);
+void ShowArray(int *arr);
+
+int main(void)
+{
+    int arr[SIZE] = {1, -2, 3, -78, 9, 12, -3, 14, 34, 15};
+    puts("The array:");
+    ShowArray(arr);
+    puts("The array after calling merge sort:");
+    MergeSort(arr, 0, SIZE-1);
+    ShowArray(arr);
+
+    return 0;
+}
+
+void Merge(int *arr, int left, int middle, int right)
+{
+    int left_size = middle-left;
+    int right_size = right-middle+1;
+    int left_arr[left_size];
+    int right_arr[right_size];
+    int i, j, k;
+    for (i=left; i<left_size; i++)
+    {
+        left_arr[i-left] = arr[i];
+    }
+    for (j=middle; j<=right_size; j++)
+    {
+        right_arr[j-middle] = arr[j];
+    }
+    //对两个子数组元素进行合并
+    i=0; j=0; k=left;
+    while (i<left_size && j<right_size)
+    {
+        if (left_arr[i] < right_arr[j])
+        {
+            arr[k] = left_arr[i];
+            i++;
+            k++;
+        }
+        else
+        {
+            arr[k] = right_arr[j];
+            j++;
+            k++;
+        }
+    }
+    while (i < left_size)
+    {
+        arr[k] = left_arr[i];
+        i++;
+        k++;
+    }
+    while (j < right_size)
+    {
+        arr[k] = right_arr[j];
+        j++;
+        k++;
+    }
+}
+
+void MergeSort(int *arr, int left, int right)
+{
+    if (left == right)
+        return;
+    else
+    {
+        int middle = (left+right)/2;
+        MergeSort(arr, left, middle);
+        MergeSort(arr, middle+1, right);
+        Merge(arr, left, middle+1, right);
+    }
+}
+
+void ShowArray(int *arr)
+{
+    for (int i=0; i<SIZE; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    putchar('\n');
+}
