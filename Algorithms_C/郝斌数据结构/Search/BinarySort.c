e8e5997b16e95acd619847a75db94a054f61a267
keviness <kevin1737@163.com>
2020-08-25 16:44:39 +0800

    Fix: binary sort

--- Algorithms_C/郝斌数据结构/Search/BinarySort.c
+++ Algorithms_C/郝斌数据结构/Search/BinarySort.c
@@ -7,7 +7,7 @@ int BinarySearch(int *arr, int value, int length);
 
 int main(void)
 {
-    int arr[SIZE] = {1, -2, -23, 23, 12, 10, -34, 25, 12};
+    int arr[SIZE] = {-23, -3, 1, 2, 10, 13, 25, 22, 45}; //数组必须有序
     int value;
     char ch;
     puts("Enter the value you want to search:");
@@ -19,35 +19,34 @@ int main(void)
         }
         puts(" is not a number! try again:");
     }
-    while(getchar() != '\n') 
-        continue;
+    while (getchar() != '\n')continue;
     printf("The value: %d is in %d index \n", value, BinarySearch(arr, value, SIZE));
 
     return 0;
 }
 
+/*** 非递归版本 ***/
 int BinarySearch(int *a, int value, int n)
 {
-    int low, high, mid;
+    int low, high;
     low = 0;
     high = n-1;
-    int result;
-    while(low<=high)
+
+    while(low <= high)
     {
-        mid = (low+high)/2;
-        if(a[mid]==value)
-            result = mid;
-        if(a[mid]>value)
+        int mid = (low+high)/2;
+        printf("low:%d\n", low);
+        printf("high:%d\n", high);
+        printf("middle:%d\n", mid);
+        if(a[mid] == value)
+            return mid;
+        if(a[mid] > value)
             high = mid-1;
-        if(a[mid]<value)
+        if(a[mid] < value)
             low = mid+1;
     }
-    if (low > high)
-        result = -1;
-    return result;
+    return -1;
 }
-
-/*** 非递归版本 ***/
 
 /*** 递归实现 ***/
 /*
