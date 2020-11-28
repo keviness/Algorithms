7c4c2d43e738577caad553d7641a872cf6daf3e4
keviness <kevin1737@163.com>
2020-11-28 22:43:07 +0800

    Update: Array

--- Algorithms_Java/DataStruct/Array/Array.java
+++ Algorithms_Java/DataStruct/Array/Array.java
@@ -9,17 +9,25 @@ public class Array
 {
     public static void main(String[] args)
     {
-        ArrayList person = new ArrayList(5);
-
+        ArrayList personList = new ArrayList(5);
+        Node a1 = new Node(12);
+        Node a2 = new Node(13);
+        System.out.println(a1);
+        System.out.println(a2);
+        System.out.println(personList.length);
+        personList.append(a1);
+        //personList.append(a2);
+        //personList.show();
     }
 }
 
 class Node
 {
-    private int data;
+    public int data;
+
     public Node(int data)
     {
-        this.data = data
+        this.data = data;
     }
 
     public String toString()
@@ -32,16 +40,16 @@ class ArrayList
 {
     public int length;
     public int count;
-    public Node[] array = new Node[length];
     
     public ArrayList(int length)
     {
         this.length = length;
     }
+    public Node[] array = new Node[this.length];
     
     public boolean isEmpty()
     {
-        return this.length == 0;
+        return this.count == 0;
     }
 
     public int get(int index)
@@ -49,7 +57,7 @@ class ArrayList
         if (this.isEmpty())
         {
             System.out.println("The array is empty!");
-            return;
+            return -1;
         }
         return this.array[index].data;
     }
@@ -66,7 +74,13 @@ class ArrayList
 
     public void append(Node element)
     {
-        array[length-1] = element;
+        if (isEmpty())
+        {
+            this.array[count] = element;
+            return;
+        }
+        this.array[this.count-1] = element;
+        this.count++;
     }
 
     public void insert(int index, Node value)
@@ -75,19 +89,29 @@ class ArrayList
         {
             this.array[0] = value;
         }
-        else if(index<1 && index>length+1)
+        else if(index<1 && index>count+1)
         {
             System.out.println("The error index!");
             return;
         }
         else
         {
-            for (int i=this.count; i>=index; i--)
+            int i = 0;
+            for (i=this.count; i>=index; i--)
             {
                 this.array[i+1] = this.array[i];
             }
             this.array[i-1] = value;
+            this.count++;
         }
     } 
+
+    public void show()
+    {
+        for (int i=0; i<this.count; i++)
+        {
+            System.out.println(array[i]);
+        }
+    }
 }
 
