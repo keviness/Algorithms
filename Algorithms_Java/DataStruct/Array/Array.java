9eabe2a1537c118d6f1918db69b7bf04f880a333
keviness <kevin1737@163.com>
2020-11-29 10:03:26 +0800

    Update: Array

--- Algorithms_Java/DataStruct/Array/Array.java
+++ Algorithms_Java/DataStruct/Array/Array.java
@@ -9,15 +9,23 @@ public class Array
 {
     public static void main(String[] args)
     {
-        ArrayList personList = new ArrayList(5);
+        ArrayList personList = new ArrayList();
         Node a1 = new Node(12);
         Node a2 = new Node(13);
+        Node a3 = new Node(16);
+        Node a4 = new Node(20);
+
         System.out.println(a1);
         System.out.println(a2);
         System.out.println(personList.length);
+        //(personList.array)[0] = a1;
+        //personList.array[1] = a2;
         personList.append(a1);
-        //personList.append(a2);
-        //personList.show();
+        personList.append(a2);
+        personList.append(a4);
+        personList.show();
+        personList.insert(2, a3);
+        personList.show();
     }
 }
 
@@ -38,18 +46,19 @@ class Node
 
 class ArrayList
 {
-    public int length;
+    public int length = 5;
     public int count;
     
-    public ArrayList(int length)
-    {
-        this.length = length;
-    }
     public Node[] array = new Node[this.length];
     
     public boolean isEmpty()
     {
-        return this.count == 0;
+        return this.count == 0? true:false;
+    }
+
+    public boolean isFull()
+    {
+        return this.count == this.length? true:false;
     }
 
     public int get(int index)
@@ -74,12 +83,12 @@ class ArrayList
 
     public void append(Node element)
     {
-        if (isEmpty())
+        if (this.isFull())
         {
-            this.array[count] = element;
+            System.out.println("The array is full!");
             return;
         }
-        this.array[this.count-1] = element;
+        this.array[this.count] = element;
         this.count++;
     }
 
@@ -89,6 +98,11 @@ class ArrayList
         {
             this.array[0] = value;
         }
+        else if (this.isFull())
+        {
+            System.out.println("The array is full!");
+            return;
+        }
         else if(index<1 && index>count+1)
         {
             System.out.println("The error index!");
@@ -96,21 +110,21 @@ class ArrayList
         }
         else
         {
-            int i = 0;
-            for (i=this.count; i>=index; i--)
+            for (int i=this.count-1; i>=index-1; i--)
             {
                 this.array[i+1] = this.array[i];
             }
-            this.array[i-1] = value;
+            this.array[index] = value;
             this.count++;
         }
     } 
 
     public void show()
     {
+        System.out.println("The array elements:");
         for (int i=0; i<this.count; i++)
         {
-            System.out.println(array[i]);
+            System.out.println(i+1 + ": "+ array[i]);
         }
     }
 }
